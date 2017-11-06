430abcb463d1bd403af018e91fa8b47ea5517e5c
Yosvaldy Fernandez <yosvaldy2011@gmail.com>
2017-11-06 15:59:07 -0500

    Configuring CamelNotation for Json result and Formatting the Media Type.

--- EmploymentSolution/Appplication.Api/App_Start/WebApiConfig.cs
+++ EmploymentSolution/Appplication.Api/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace Appplication.Api
@@ -9,7 +10,23 @@ namespace Appplication.Api
     {
         public static void Register(HttpConfiguration config)
         {
+            //CamelNotation
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
             // Web API configuration and services
+
+
+            //ToFormatMediaType
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings
+                .Add(new RequestHeaderMapping(
+                    "Accept",
+                    "text/html",
+                    StringComparison.InvariantCultureIgnoreCase,
+                    true,
+                    "application/json"
+                ));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
