4169bd14d8f5790a8723a241aff1a15569fd6151
Yosvaldy Fernandez <yosvaldy2011@gmail.com>
2017-11-08 14:10:40 -0500

    Remove Application.ViewModel project from Solution

--- EmploymentSolution/Application.Service/Abstract/ICompanyService.cs
+++ EmploymentSolution/Application.Service/Abstract/ICompanyService.cs
@@ -1,10 +1,5 @@
 ﻿using Application.Dtos;
-using Application.ViewModels;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Service.Abstract
 {
