5bd0df1f66aba6b6e1c9a4744fee89e6bb08f9c9
Yosvaldy Fernandez <yosvaldy2011@gmail.com>
2017-11-09 01:49:12 -0500

    Fixing a bug

--- EmploymentSolution/Application.Service/Infrastructure/Mapping/ModelToDto.cs
+++ EmploymentSolution/Application.Service/Infrastructure/Mapping/ModelToDto.cs
@@ -17,7 +17,7 @@ namespace Application.Service.Infrastructure.Mapping
             CreateMap<Employment, EmploymentDto>()
                 .ForMember(cr => cr.Employee, opt => opt.MapFrom(c => new EmployeeDto { FullName = c.FullName, Email = c.Email, Phone = c.Phone }))
                 .ForMember(cr => cr.Accesses, opt => opt.MapFrom(c => c.Accesses.Select(e => e.AccessId)))
-                .ForMember(cr => cr.Equipments, opt => opt.MapFrom(c => c.Equipments.Select(e => e.EmploymentId)));
+                .ForMember(cr => cr.Equipments, opt => opt.MapFrom(c => c.Equipments.Select(e => e.EquipmentId)));
 
             CreateMap<Company, CompanyDto>();
             CreateMap<Position, PositionDto>();
